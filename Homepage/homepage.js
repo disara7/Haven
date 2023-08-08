3e28e96ee23707fcb87bc64e1500165c491b3873
disara7 <129250994+disara7@users.noreply.github.com>
2023-08-08 14:55:13 +0530

    image links updated

--- Homepage/homepage.js
+++ Homepage/homepage.js
@@ -1,60 +1,11 @@
-let playBtn = document.querySelectorAll('.playlist .box-container .box .play');
-let musicPlayer = document.querySelector('.music-player');
-let musicAlbum = musicPlayer.querySelector('.album');
-let musicName = musicPlayer.querySelector('.name');
-let musicArtist = musicPlayer.querySelector('.artist');
-let music = musicPlayer.querySelector('.music');
-
-playBtn.forEach(play =>{
-
-   play.onclick = () =>{
-      let src = play.getAttribute('data-src');
-      let box = play.parentElement.parentElement;
-      let name = box.querySelector('.name');
-      let album = box.querySelector('.album');
-      let artist = box.querySelector('.artist');
-
-      musicAlbum.src = album.src;
-      musicName.innerHTML = name.innerHTML;
-      musicArtist.innerHTML = artist.innerHTML;
-      music.src = src;
-
-      musicPlayer.classList.add('active');
-
-      music.play();
-
-   }
-
-});
-
-document.querySelector('#close').onclick = () =>{
-   musicPlayer.classList.remove('active');
-   music.pause();
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";  
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-}
+//TESTIMONIAL SLIDER
+if ($(".ts-testimonial-slide").length > 0) {
+    
+  $(".ts-testimonial-slide").owlCarousel({
+      autoPlay: 4000,
+      slideSpeed: 1000,
+      navigation: false,
+      pagination: true,
+      singleItem: true
+  });
+};
